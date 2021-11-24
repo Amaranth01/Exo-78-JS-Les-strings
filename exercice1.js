8dc212e82ab7e48b30a3315d74d136a2360e33e9
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-24 18:47:04 +0100

    Exo 78 ok

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,21 @@
-let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
+    "dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let divPos = document.getElementById('position');
+let position = monTexte.indexOf('i');
 
+divPos.innerHTML = position.toString()
 
+let chaineMaj = document.getElementById('chaineMaj');
+let monNouveauTexte = monTexte.toUpperCase();
 
-
-
+chaineMaj.innerHTML = monNouveauTexte;
 
 
